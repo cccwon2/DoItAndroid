a67f90dd4f2965612c9ff2b9a0f3a07616bcf9a8
bbuzzart <wonkim@bbuzzart.com>
2016-06-09 15:35:42 +0900

    DoItMission02 마무리

--- DoItMission02/app/src/main/java/me/memorytalk/doitmission02/MainActivity.java
+++ DoItMission02/app/src/main/java/me/memorytalk/doitmission02/MainActivity.java
@@ -1,13 +1,59 @@
 package me.memorytalk.doitmission02;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    EditText editText;
+    TextView textView;
+
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.d(TAG, "beforeTextChanged() : " + s);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textView.setText(String.valueOf(s.length()));
+                Log.d(TAG, "onTextChanged() : " + s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.d(TAG, "afterTextChanged() : " + s);
+            }
+        });
+    }
+
+    public void onButton1Clicked(View v) {
+        toast = Toast.makeText(getApplicationContext(), editText.getText(), Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void onButton2Clicked(View v) {
+        if (toast != null) {
+            toast.cancel();
+        }
     }
 }
