cb7f77c0703cb8ec779574eecee4f96d69f2b193
bbuzzart <wonkim@bbuzzart.com>
2016-06-09 11:52:29 +0900

    DoItMission01 마무리

--- DoItMission01/app/src/main/java/me/memorytalk/doitmission01/MainActivity.java
+++ DoItMission01/app/src/main/java/me/memorytalk/doitmission01/MainActivity.java
@@ -1,13 +1,31 @@
 package me.memorytalk.doitmission01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView1;
+    ImageView imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+    }
+
+    public void onImageButton1Clicked(View v) {
+        imageView1.setImageResource(R.drawable.picture01);
+        imageView2.setImageBitmap(null);
+    }
+
+    public void onImageButton2Clicked(View v) {
+        imageView1.setImageBitmap(null);
+        imageView2.setImageResource(R.drawable.picture01);
     }
 }
